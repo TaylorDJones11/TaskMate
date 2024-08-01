254066656636113c1e491ae01487a38cbeed0a0f
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-08-01 16:59:05 -0500

    adding form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
 function Form() {
+  const [value, setValue] = useState('');
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(value);
+  }
+
   return (
-    <form>
+    <form className='todo-form' onSubmit={handleSubmit}>
       <input
         type='text'
         className='todo-input'
         placeholder='What is the task toaday?'
+        onChange={handleChange}
       />
-      <button className='todo-btn'>Submit</button>
+      <button className='todo-btn'>Add Task</button>
     </form>
   );
 }
--- src/components/ToDoWrapper.jsx
+++ src/components/ToDoWrapper.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import Form from './Form';
+import { nanoid } from 'nanoid';
 
 function ToDoWrapper() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo({ todo }) {
+    setTodos([
+      ...todos,
+      { id: nanoid(), task: todo, completed: false, isEditing: false },
+    ]);
+  }
   return (
-    <div>
-      <Form />
+    <div className='TodoWrapper'>
+      <Form addTodo={addTodo} />
     </div>
   );
 }
